9c253492d8e4c13d6fcd50556da620302d0e33b9
cksdid202 <141496725+cksdid202@users.noreply.github.com>
2026-01-28 17:17:24 +0900

    [Bronze I] Title: 세로읽기, Time: 108 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		char [][] arr = new char[5][15];
+		for(int i=0; i < 5; i++) {
+			String line = br.readLine();
+			
+			for(int j=0; j<line.length();j++) {
+				arr[i][j] = line.charAt(j);
+			}
+		}
+		for(int i = 0; i < 15; i++) {
+			for(int j = 0; j < 5; j++) {
+				if(arr[j][i] != '\0') {
+					sb.append(arr[j][i]);
+				}
+				
+			}
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+}
