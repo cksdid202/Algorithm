29c77ba0446b1ed9c62c9d5bfe7fe9b4531f5c2a
cksdid202 <141496725+cksdid202@users.noreply.github.com>
2026-02-11 14:25:43 +0900

    [D2] Title: 파리 퇴치, Time: 152 ms, Memory: 24,960 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,4 +1,3 @@
-
 import java.util.*;
 import java.io.*;
 
@@ -7,41 +6,42 @@ public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        int T = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
         
-        for(int t = 1; t <= T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int M = Integer.parseInt(st.nextToken());
-            
-            int[][] S = new int[N + 1][N + 1];
-            
-            for(int i = 1; i <= N; i++) {
-                st = new StringTokenizer(br.readLine());
-                for(int j = 1; j <= N; j++) {
-                    int val = Integer.parseInt(st.nextToken());
-                    
-                    S[i][j] = S[i-1][j] + S[i][j-1] - S[i-1][j-1] + val;
-                }
-            }
-            
-            int max = 0;
-            
-            for(int i = M; i <= N; i++) {
-                for(int j = M; j <= N; j++) {
-                    
-                    int sum = S[i][j] - S[i-M][j] - S[i][j-M] + S[i-M][j-M];
-                    
-                    if(max < sum) {
-                        max = sum;
-                    }
-                }
-            }
-            
-            sb.append("#").append(t).append(" ").append(max).append("\n");
+        for(int t = 0; t < T; t++) {
+        	
+        	st = new StringTokenizer(br.readLine());
+        	
+        	int N = Integer.parseInt(st.nextToken());
+        	int M = Integer.parseInt(st.nextToken());
+        	
+        	int[][] map = new int[N+1][N+1];
+        	
+        	for(int i = 1; i <= N; i++) {
+        		st = new StringTokenizer(br.readLine());
+        		for(int j = 1; j <= N; j++) {
+        			int value = Integer.parseInt(st.nextToken());
+        			map[i][j] = map[i-1][j] + map[i][j-1] - map[i-1][j-1] + value;
+        		}
+        	}
+        	
+        	int max = 0;
+        	for(int i = M; i <= N; i++) {
+        		for(int j = M; j<= N; j++) {
+        			int result = map[i][j] - map[i-M][j] - map[i][j-M] + map[i-M][j-M];
+        			if(result > max) {
+        				max =result;
+        			}
+        		}
+        	}
+        	
+        	sb.append("#").append(t+1).append(" ").append(max).append("\n");
+        	
+        	
         }
+        System.out.print(sb);
         
-        System.out.println(sb);
     }
 }
