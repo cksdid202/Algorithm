41c6862c286d9cbd1b6f19e00a362d419657696e
cksdid202 <141496725+cksdid202@users.noreply.github.com>
2026-03-19 17:11:52 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,49 +1,57 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    
-    static int N;
-    static int[] T; 
-    static int[] P; 
-    static int maxProfit = 0; 
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        N = Integer.parseInt(br.readLine());
-        T = new int[N];
-        P = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            T[i] = Integer.parseInt(st.nextToken());
-            P[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        dfs(0, 0);
-        
-        System.out.println(maxProfit);
-    }
-    
-    static void dfs(int day, int profit) {
-        
-        if (day == N) {
-            maxProfit = Math.max(maxProfit, profit);
-            return;
-        }
-        
-        if (day > N) {
-            return;
-        }
-        
-       
-        
-        // 상담 O
-        dfs(day + T[day], profit + P[day]);
-        
-        // 상담 X
-        dfs(day + 1, profit);
-    }
+	static int N;
+	static int[][] pyo;
+	static int maxBenefit = Integer.MIN_VALUE;
+		
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		pyo = new int[N+1][2];
+		
+		for(int i = 1; i <= N; i++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			pyo[i][0] = Integer.parseInt(st.nextToken());
+			pyo[i][1] = Integer.parseInt(st.nextToken());
+			
+			
+		}
+		
+		subset(1, 0, 0);
+		
+		System.out.print(maxBenefit);
+		
+		
+		
+	}
+	
+	public static void subset(int diff, int pay, int cnt) {
+	    
+	    if(diff > N + 1) {
+	        return;
+	    }
+	    
+	    if(maxBenefit < pay) {
+	        maxBenefit = pay;
+	    }
+	    
+	    if(diff == N + 1) {
+	        return;
+	    }
+	    
+	    subset(diff + pyo[diff][0], pay + pyo[diff][1], cnt + 1);
+	    subset(diff + 1, pay, cnt);
+	}
+	
+	
+
 }
