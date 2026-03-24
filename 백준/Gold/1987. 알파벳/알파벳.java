ce16c44fef51af3875b10c9dad28a72079701963
cksdid202 <141496725+cksdid202@users.noreply.github.com>
2026-03-24 14:09:09 +0900

    [Gold IV] Title: 알파벳, Time: 896 ms, Memory: 14980 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,83 +1,60 @@
-
 import java.util.*;
 import java.io.*;
 
 public class Main {
 	
-	static int R, C, cnt, maxCnt=0;
+	static int R, C, maxCnt = 0;
 	static char[][] map;
-	static boolean[][] visited;
-	static char[] charlist;
-	static int[] dr = {-1, 1, 0, 0}; // 상 하 좌 우
+    
+	static boolean[] alphaCheck = new boolean[26]; 
+    
+	static int[] dr = {-1, 1, 0, 0};
 	static int[] dc = {0, 0, -1, 1};
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
 		map = new char[R][C];
-		visited = new boolean[R][C];
-		charlist = new char[R*C];
-		
 		
 		for(int i = 0; i < R; i++) {
 			String str = br.readLine();
 			for(int j = 0; j < C; j++) {
-				map[i][j] = str.charAt(j); 
+				map[i][j] = str.charAt(j);
 			}
 		}
-		visited[0][0] = true;
-		charlist[0] = map[0][0];
 		
-		bfs(0, 0, visited, 1);
+		alphaCheck[map[0][0] - 'A'] = true;
+		
+		dfs(0, 0, 1); // 1칸 시작
 		
 		System.out.print(maxCnt);
-		
-		
-		
 	}
 	
-	public static void bfs(int r, int c, boolean[][] visited, int cnt) {
+	public static void dfs(int r, int c, int cnt) {
 		
 		if(cnt > maxCnt) {
 			maxCnt = cnt;
 		}
 		
 		for(int i = 0; i < 4; i++) {
-			
 			int nr = r + dr[i];
 			int nc = c + dc[i];
 			
 			if(nr >= 0 && nr < R && nc >= 0 && nc < C) {
-				if(!visited[nr][nc]) {
-					int flag = 0;
-					for(int j = 0; j < cnt; j++) {
-						if(map[nr][nc] == charlist[j] ) {
-							flag = 1;
-							break;
-						}
-					}
-					if(flag == 0) {
-							visited[nr][nc] = true;
-							charlist[cnt] = map[nr][nc];
-							bfs(nr, nc, visited, cnt+1);
-							visited[nr][nc] = false;
-					}
-					
-					
+                // 다음 칸 숫자 변환
+                int nextAlpha = map[nr][nc] - 'A';
+                
+                // 배열 인덱스로 검사
+				if(!alphaCheck[nextAlpha]) {
+                    alphaCheck[nextAlpha] = true;  
+                    dfs(nr, nc, cnt + 1);          
+                    alphaCheck[nextAlpha] = false; 
 				}
 			}
-			
 		}
-		
-		
 	}
-	
-
 }
