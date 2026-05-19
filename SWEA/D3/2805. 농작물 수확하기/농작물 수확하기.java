6ebb1f0c790b2aefe51f3b4a3f69efac8c74405b
cksdid202 <141496725+cksdid202@users.noreply.github.com>
2026-05-19 17:45:41 +0900

    [D3] Title: 농작물 수확하기, Time: 88 ms, Memory: 27,008 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -3,53 +3,61 @@ import java.util.*;
 import java.io.*;
 
 public class Solution {
-
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		
-		for(int t = 0; t < T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			int[][] arr = new int[N][N];
-			
-			for(int i = 0; i < N; i++) {
-				String line = br.readLine();
-				for(int j = 0; j < N; j++) {
-					arr[i][j] = line.charAt(j) - '0';
-				}
-			}
-			
-			int cnt = 0;
-						int start = N / 2;
-						int end = N / 2;
-						
-						for(int i = 0; i < N; i++) {
-							
-							for(int j = start; j <= end; j++) {
-								cnt += arr[i][j];
-							}
-							// 중간부터는 줄이기 : 다이아몬드
-							if (i < N / 2) {
-								start--; 
-								end++;   
-							} else {
-								start++; 
-								end--;   
-							}
-						}
-						
-						sb.append("#").append(t + 1).append(" ").append(cnt).append("\n");
-			
-		}
-		
-		System.out.println(sb);
-		
-		
-	}
-
+    public static void main(String[] args) throws Exception {
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	
+    	int T = Integer.parseInt(br.readLine());
+    	
+    	for(int t = 1; t <= T; t++) {
+    		
+    		int N = Integer.parseInt(br.readLine());
+    		
+    		int[][] prefixSum = new int[N+1][N+1];
+    		
+    		for(int i = 1; i <= N; i++) {
+    			String line = br.readLine();
+    			for(int j = 1; j <= N; j++) {
+    				int value = line.charAt(j - 1) - '0';
+    				
+    				prefixSum[i][j] = prefixSum[i][j-1] + value;
+    			}
+    		}
+    		
+    		int totalHarvest = 0;
+    		int mid = N / 2 + 1;
+    		int range = 0;
+    		
+    		for (int i = 1; i <= N; i++) {
+    			
+    			
+    			int start = mid - range;
+    			int end = mid + range;
+    			
+    			totalHarvest += (prefixSum[i][end] - prefixSum[i][start - 1]);
+    			
+    			if (i < mid) {
+    				range++;
+    			}
+    			else {
+    				range--;
+    			}
+    			
+    			
+    			
+    		}
+    		
+    		sb.append('#').append(t).append(' ').append(totalHarvest).append('\n');
+    		
+    		
+    		
+    		
+    		
+    		
+    	}
+    	System.out.print(sb);
+        
+    	
+    }
 }
