dc8d8f4a5623ab719040c8010f848580424fd7eb
cksdid202 <141496725+cksdid202@users.noreply.github.com>
2026-05-21 15:39:02 +0900

    [level 2] Title: 구명보트, Time: 5.46 ms, Memory: 56.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        
+        while(left <= right) {
+            
+            if(people[left] + people[right] <= limit) {
+                
+                left++;
+                right--;
+                
+            }
+            else {
+                right--;
+            }
+            answer++;
+            
+        }
+        
+        
+        
+        
+        return answer;
+    }
+}
