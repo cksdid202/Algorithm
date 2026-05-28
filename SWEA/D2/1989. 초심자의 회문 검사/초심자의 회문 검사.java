9ff421d4c10c8628c8913d72bad3e067f5417884
cksdid202 <141496725+cksdid202@users.noreply.github.com>
2026-05-28 10:33:21 +0900

    [D2] Title: 초심자의 회문 검사, Time: 74 ms, Memory: 22,144 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -2,9 +2,9 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Solution{
+public class Solution {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,19 +16,20 @@ public class Solution{
 			
 			String line = br.readLine();
 			
-			int len = line.length();
-			String reverse = "";
+			int result = 1;
 			
-			for(int i = len-1; i >= 0; i--) {
-				reverse += line.charAt(i);
-			}
-			
-			int result = 0;
-			if(line.equals(reverse)) {
-				result = 1;
+			for(int i = 0; i <= line.length() / 2; i++) {
+				
+				if(line.charAt(i) != line.charAt(line.length()-1-i)) {
+					result = 0;
+					break;
+				}
+				
 			}
 			
 			sb.append('#').append(t).append(' ').append(result).append('\n');
+			
+			
 			
 		}
 		System.out.print(sb);
